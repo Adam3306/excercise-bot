98ac2eef0ec8ccd43b181a0e5555ad225637d91a
Adam Magyar <adam.magyar@vodafone.com>
2023-02-01 21:26:55 +0100

    refactor

--- src/templates/sendGenericTemplate.js
+++ src/templates/sendGenericTemplate.js
@@ -1,33 +1,7 @@
 const request = require("request");
-module.exports = function sendGenericTemplate(recipientId, respBody) {
-  console.log(respBody);
-  const nutritionalValue = [];
-  for (let i = 0; i < respBody.length; i++) {
-    // I dont like using forEach
-    let obj = {
-      title: respBody[i].food_name,
-      image_url: respBody[i].thumbnail,
-      subtitle:
-        "Total Calories: " +
-        respBody[i].total_calories +
-        "\n" +
-        "protein: " +
-        respBody[i].protein +
-        "\n" +
-        "Carbohydrates: " +
-        respBody[i].total_carbohydrate,
-    };
-    nutritionalValue.push(obj);
-  }
-  let messageData = {
-    attachment: {
-      type: "template",
-      payload: {
-        template_type: "generic",
-        elements: nutritionalValue,
-      },
-    },
-  };
+module.exports = function sendGenericTemplate(recipientId) {
+  console.log("recipientId is \t", recipientId);
+
   request(
     {
       url: "https://graph.facebook.com/v2.6/me/messages",
@@ -35,7 +9,7 @@ module.exports = function sendGenericTemplate(recipientId, respBody) {
       method: "POST",
       json: {
         recipient: { id: recipientId },
-        message: messageData,
+        message: "hello world",
       },
     },
     function (error, response, body) {
